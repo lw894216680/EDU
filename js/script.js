a6f916d8a200c0613318d82da422ba6e3088ada2
Lizhg Huang <tohlz@outlook.com>
2016-03-12 13:32:24 +0800

    slide v1.0

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 /* banner 轮播图 */
+
+// 图片渐变
 var slide = {
     fadeout: function (ele,stepLength,stepTime) {
         if (!parseFloat(ele.style.opacity)) {
@@ -27,80 +29,44 @@ var slide = {
             }
         }
         var setfadein = setInterval(step, 10);
-    },
-    msover: function(ele,setIntervalId) {
-        ele.style.opacity = 1;
-        clearInterval(setIntervalId);
-    },
-    msout: function(setIntervalId,func,time) {
-        setIntervalId = setInterval(func,time);
     }
 }
 
-// function fadeout (ele) {
-//     var stepLength = 1/50;
-//     if (!parseFloat(ele.style.opacity)) {
-//         ele.style.opacity = 1;
-//     }
-//     function step () {
-//         if (parseFloat(ele.style.opacity)-stepLength > 0) {
-//             ele.style.opacity = parseFloat(ele.style.opacity)-stepLength;
-//         } else {
-//             ele.style.opacity = 0;
-//             clearInterval(setIntervalId);
-//         }
-//     }
-//     var setIntervalId = setInterval(step, 10);
+// 获取下一张轮播图节点
+function getAfterSlide(arr,crtSlide) {
+    for (var i=0;i<arr.length;i++) {
+        if (crtSlide === arr[i]) {
+            var sldIndex = i;
+            break;
+        }
+    }        
+    if (sldIndex === (arr.length-1)) {
+        return arr[0];
+    }else {
+        return arr[sldIndex + 1];
+    }        
+} 
 
-// }
-
-// function fadein (ele) {
-//     var stepLength = 1/50;
-//     if (!parseFloat(ele.style.opacity)) {
-//         ele.style.opacity = 0;
-//     }
-//     function step () {
-//         if (parseFloat(ele.style.opacity)+ stepLength < 1) {
-//             ele.style.opacity = parseFloat(ele.style.opacity)+stepLength;
-//         } else {
-//             ele.style.opacity = 1;
-//             clearInterval(setIntervalId);
-//         }
-//     }
-//     var setIntervalId = setInterval(step, 10);
-// }
-
+// 轮播函数
 function slider() {
-            var slides = getByClass('slide'),
-            crtSlide = getByClass('crt_slide')[0];  
-    function animation() {
-        var slides = getByClass('slide'),
-            crtSlide = getByClass('crt_slide')[0];        
-        var sldIndex, afterIndex;
-        for (var i=0;i<slides.length;i++) {
-            if (crtSlide === slides[i]) {
-                sldIndex = i;
-                break;
-            }
-        }
-        if (sldIndex === 0) {
-            afterIndex = 1;
-        } else if (sldIndex === 1) {
-            afterIndex = 2;
-        } else {
-            afterIndex = 0;
-        }
-        var afterSlide = slides[afterIndex];
+    var slides = getByClass('slide'),
+        crtSlide = getByClass('crt_slide')[0];
+
+    function animation() {      
+        var afterSlide = getAfterSlide(slides, crtSlide);
 
         slide.fadein(afterSlide,1/50,10);
         slide.fadeout(crtSlide,1/50,10);
         crtSlide.className = "slide";
         afterSlide.className += " crt_slide";
-    }    
-    var onload = setInterval(animation,5000);
-    eventUtil.addHandler(crtSlide, 'mouseover', function(){clearInterval(onload)});
-    eventUtil.addHandler(crtSlide, 'mouseout', function(){onload = setInterval(animation,5000);});
+        crtSlide = afterSlide;
+    }
 
+    var onload = setInterval(animation,5000);
+    for (var i=0;i<slides.length;i++) {   
+        eventUtil.addHandler(slides[i], 'mouseover', function(){clearInterval(onload);});
+        eventUtil.addHandler(slides[i], 'mouseout', function(){onload = setInterval(animation,5000);});
+    }
 }
 
 // pointer指示器
